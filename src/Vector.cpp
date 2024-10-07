10042fe5bc7a7fda2babf8ff1677b9e547723a13
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-10-08 04:05:11 +0600

    Implemented copy constructor in vector class

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -3,16 +3,26 @@
 template<typename T>
 class Vector{
 private:
-	T* arr;
-	size_t size;
-	size_t capacity;
+	T* arr; //main vector array
+	size_t size; //current number of element
+	size_t capacity; //capacity of array
 
-	Vector(const Vector& other) {
+public:
+	//constructor
+	Vector(size_t initialCapacity = 2) : size(0), capacity(initialCapacity) {
+        arr = new T[capacity];
+    }
+    //copy constructor
+    Vector(const Vector& other) {
         size = other.size;
         capacity = other.capacity;
         arr = new T[capacity];
         for (size_t i = 0; i < size; ++i) {
             arr[i] = other.arr[i];
         }
+    }
+    //destructor
+    ~Vector() {
+        delete[] arr;
     }
 }
