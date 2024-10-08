4b49a1420f89816b304d8b3815ab0585327d63ad
Muhtasim Zawad <muhtasimzawad.jfcl@gmail.com>
2024-10-08 21:31:25 +0600

    implemented resize, constructor, copy constructor and destructor in vector class

---
+++ src/Vector.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+template <typename T>
+class Vector {
+    private:
+        T* arr;          // Array to store the elements
+        size_t size;     // Current number of elements
+        size_t capacity; // Capacity of the array
+
+        // Resizes the array when needed
+        void resize() {
+            capacity *= 2;
+            T* newArr = new T[capacity];
+            for (size_t i = 0; i < size; ++i) {
+                newArr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+        }
+
+    public:
+        // Constructor
+        Vector(size_t initialCapacity = 2) : size(0), capacity(initialCapacity) {
+            arr = new T[capacity];
+        }
+
+        // Copy constructor
+        Vector(const Vector& other) {
+            size = other.size;
+            capacity = other.capacity;
+            arr = new T[capacity];
+            for (size_t i = 0; i < size; ++i) {
+                arr[i] = other.arr[i];
+            }
+        }
+
+        // Destructor
+        ~Vector() {
+            delete[] arr;
+        }
+};
