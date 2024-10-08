72888345eb13fbc824a8fed456d2607bd94e145b
Muhtasim Zawad <muhtasimzawad.jfcl@gmail.com>
2024-10-09 00:15:55 +0600

    implemented sort in vector class

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -86,4 +86,17 @@ class Vector {
             }
             --size;
         }
+
+        // Sort the vector elements
+        void sort() {
+            for (size_t i = 0; i < size - 1; ++i) {
+                    for (size_t j = 0; j < size - i - 1; ++j) {
+                        if (arr[j] > arr[j + 1]) {
+                            T temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
+        }
 };
