b97ea9c36eff802bdd1b8df099afd35b414c48c3
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-10-08 09:48:24 +0600

    implemented push_back

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -7,6 +7,17 @@ private:
 	size_t size; //current number of element
 	size_t capacity; //capacity of array
 
+    //resizes the array for push_back
+    void resize() {
+        capacity *= 2;
+        T* newArr = new T[capacity];
+        for (size_t i = 0; i < size; ++i) {
+            newArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = newArr;
+    }
+
 public:
 	//constructor
 	Vector(size_t initialCapacity = 2) : size(0), capacity(initialCapacity) {
@@ -24,5 +35,25 @@ public:
     //destructor
     ~Vector() {
         delete[] arr;
+    }
+    //vector assignment
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            delete[] arr;
+            size = other.size;
+            capacity = other.capacity;
+            arr = new T[capacity];
+            for (size_t i = 0; i < size; ++i) {
+                arr[i] = other.arr[i];
+            }
+        }
+        return *this;
+    }
+    //push back implementation
+    void push_back(const T& value) {
+        if (size == capacity) {
+            resize();
+        }
+        arr[size++] = value;
     }
 }
