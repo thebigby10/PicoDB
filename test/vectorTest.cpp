502e7a22a928e0bef153462eeb55dafcdac19f17
Muhtasim Zawad <muhtasimzawad.jfcl@gmail.com>
2024-10-09 18:50:37 +0600

    created vectorTest file for testing Vector File and tested push_back

---
+++ test/vectorTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../src/Vector.cpp"
+
+int main() {
+    Vector<int> vec;
+
+    // Push back elements
+    vec.push_back(5);
+    vec.push_back(10);
+    vec.push_back(15);
+
+
+
+    return 0;
+}
