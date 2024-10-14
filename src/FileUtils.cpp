e18602d695e212132eaf1fd73a075c3a8b1c3142
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-10-14 13:16:49 +0600

    implemented fileExits function in FileUtils

--- src/FileUtils.cpp
+++ src/FileUtils.cpp
@@ -2,5 +2,15 @@
 #define FILEUTILS_H
 #include "String.cpp"
 
+//only windows and linux supported
+bool fileExists(const String& filename) {
+#ifdef _WIN32  // For Windows
+    return system((String("if exist \"") + filename + String("\" exit 0")).c_str()) == 0;
+#elif __linux__  // For Linux
+    return system((String("test -e \"") + filename + String("\"")).c_str()) == 0;
+#else
+    return false;
+#endif
+}
 
 #endif
