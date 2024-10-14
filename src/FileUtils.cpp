3402db16258449ec717c16235eb107887ade5951
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-10-14 14:45:02 +0600

    readFromFile structure

--- src/FileUtils.cpp
+++ src/FileUtils.cpp
@@ -2,15 +2,22 @@
 #define FILEUTILS_H
 #include "String.cpp"
 
+// Migrate to an oop
+
 //only windows and linux supported
+
 bool fileExists(const String& filename) {
-#ifdef _WIN32  // For Windows
-    return system((String("if exist \"") + filename + String("\" exit 0")).c_str()) == 0;
-#elif __linux__  // For Linux
-    return system((String("test -e \"") + filename + String("\"")).c_str()) == 0;
-#else
-    return false;
-#endif
+	#ifdef _WIN32  // For Windows
+	    return system((String("if exist \"") + filename + String("\" exit 0")).c_str()) == 0;
+	#elif __linux__  // For Linux
+	    return system((String("test -e \"") + filename + String("\"")).c_str()) == 0;
+	#else
+	    return false;
+	#endif
+}
+
+void readFromFile(const String& filename) {
+
 }
 
 #endif
