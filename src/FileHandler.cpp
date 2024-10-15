c8281b13316d5e0dd5b25b986e8444b61ea33e49
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-10-15 11:50:51 +0600

    migrated writeFromFile to oop in FileHandler

--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -40,6 +40,28 @@ private:
         fileStream.close();  // Close the file
         return fileContent;
     }
+    // Function to write data to a file (same as before)
+    // The 'append' flag determines if the data should overwrite (false) or append (true) to the file
+    void writeToFile(const String& data, bool append = false) {
+        std::ofstream fileStream;
+        
+        // Choose file mode based on append flag
+        if (append) {
+            fileStream.open(m_filepath.c_str(), std::ios::app);  // Open file in append mode
+        } else {
+            fileStream.open(m_filepath.c_str(), std::ios::out);  // Open file in write/overwrite mode
+        }
+        
+        if (!fileStream.is_open()) {
+            std::cerr << "Failed to open file: " << m_filepath << std::endl;
+            return;
+        }
+        
+        // Write the entire string to the file
+        fileStream << data;
+        
+        fileStream.close();  // Close the file
+    }
 };
 
 
