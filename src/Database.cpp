ebb58ae7920f8147f7d67e289b0065c4816f67aa
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-10-15 20:11:05 +0600

    updated Database.cpp constructor

--- src/Database.cpp
+++ src/Database.cpp
@@ -9,17 +9,23 @@
 class Database{
 private:
 	String db_name; 
-	String db_path = "";
+	String db_path;
+	String username;
+	String key;
+	String delimiter;
 	bool encryption = true;
 	vector<Table> db_data;
 public:
 	// Database(string db_name, bool force_create, bool encryption, string file_path )
-	Database(String db_name, String db_path, String username, String key, String table_delimiter = String(";_pico_;")){
+	Database(String db_name, String db_path, String username, String key, String table_delimiter = String(";_;pico;_;")){
 		this->db_name = db_name;
-
+		this->db_path = db_path;
+		this->username = username;
+		this->key = key;
+		this->delimiter = delimiter;
 		//check if file exists
-		FileHandler db_file = FileHandler(db_path+String("/")+db_name+String(".config")); //path to config file
-		if(db_file.fileExists()){
+		FileHandler conf_file = FileHandler(db_path+String("/")+db_name+String(".config")); //path to config file
+		if(conf_file.fileExists()){
 			// fetch config file
 			
 			// fetch raw data
@@ -29,7 +35,7 @@ public:
 		else{
 			//create config file
 			ConfigManager conf_manager(db_path);
-			conf_manager.writeConfig(db_name,db_path+String("/")+db_name+".config",table_delimiter,username )
+			conf_manager.writeConfig(db_name,db_path+String("/")+db_name+".config",table_delimiter,username );
 		}
 	};
 	// Databas e(string db_name, bool encryption);
