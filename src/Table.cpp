6985e8f7718b224017a88e3e0d68e86d3613b705
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-03 01:22:13 +0600

    completed the constructor for table

--- src/Table.cpp
+++ src/Table.cpp
@@ -11,7 +11,13 @@ private:
 	vector<vector< Cell> > table_data;
 public:
 	Table(string table_name, vector<int >rows){ //add string conditions
-	
+		this->table_name = table_name;
+		for(int i = 0; i < rows.size(); ++i){
+			vector<Cell> row;
+			row.push_back(Cell(rows[i]));
+			table_data.push_back(row);
+		}
+
 
 	}
 	 getrows(){
