f92e13c1063938205cf9839ddf8fbafcae302cc4
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-04 15:50:45 +0600

    date.cpp implementation start

---
+++ src/Date.cpp
@@ -0,0 +1,37 @@
+#ifndef DATE_H
+#define DATE_H
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Date {
+private:
+    int day;
+    int month;
+    int year;
+
+
+public:
+    // Default constructor
+    Date();
+    Date(int day, int month, int year);
+
+    // Getters
+    int getDay() const;
+    int getMonth() const;
+    int getYear() const;
+
+    // Setters
+    void setDay(int day);
+    void setMonth(int month);
+    void setYear(int year);
+
+    
+
+   
+
+
+
+
+
+}
