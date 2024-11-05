29bcfc9d6d6d9525523c5276640315a64e1b7dd3
Muhtasim Zawad <muhtasimzawad.jfcl@gmail.com>
2024-11-05 22:00:29 +0600

    added new basic ConfigManager Implementation

--- src/ConfigManager.cpp
+++ src/ConfigManager.cpp
@@ -1,1 +1,9 @@
+#ifndef MANAGECONFIG_H // Include guard start
+#define MANAGECONFIG_H
+#include "String.cpp"
+//#include "Map.cpp"
+#include "Vector.cpp"
+#include "FileHandler.cpp"
+#include "Table.cpp"
+#include <fstream>
 
