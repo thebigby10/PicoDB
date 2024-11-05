14467a058cd643ea56d8b405e35d1972980632d9
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-05 21:58:50 +0600

    .

--- src/StringVectorConverter.cpp
+++ src/StringVectorConverter.cpp
@@ -6,3 +6,20 @@
 #include "FileHandler.cpp"
 #include "Table.cpp"
 #include <fstream>
+
+
+using namespace std;
+class StringVectorConverter{
+public:
+	Vector<Vector<String>> stringToVector(String data, String delimiter){
+		Vector<Vector<String>> table_cell_data;
+
+		int start_pos = 0;
+		int str_length = data.length();
+		while (start_pos<str_length) {
+			int end_pos = start_pos;
+			while (end_pos != '\n') {
+                	++end_pos;
+			}
+
+			// Extract a single line
