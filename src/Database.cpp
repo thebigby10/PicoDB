ca853af730666961961ca23c98bccaac475adb84
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-05 22:06:45 +0600

    added new header

---
+++ src/Database.cpp
@@ -0,0 +1,12 @@
+#ifndef DATABASE_H // Include guard start
+#define DATABASE_H
+#include<iostream>
+#include<filesystem>
+#include "Vector.cpp"
+#include "Encryptor.cpp"
+#include "String.cpp"
+#include "Table.cpp"
+#include "FileHandler.cpp"
+#include "Cell.cpp"
+#include "ConfigManager.cpp"
+#include "StringVectorConverter.cpp"
