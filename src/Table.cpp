31e582fd5422d7f27c0ea7836f2a8788abcaf839
Muhtasim Zawad <muhtasimzawad.jfcl@gmail.com>
2024-11-05 22:31:22 +0600

    addde commented out class structure  of Constraints in table class

--- src/Table.cpp
+++ src/Table.cpp
@@ -6,3 +6,12 @@
 #include "Cell.cpp"
 
 using namespace std;
+
+/*
+class Constrains{
+public:
+	String constraint_key; // NOT_NULL, UNIQUE, PEIMARY_KEY, FOREIGN_KEY, DEFAULT, CHECK
+	String constraint_secondary_key; //LESS_THAN, GREATER_THAN, EQUAL_TO, NOT_EQUAL_TO, TABLE_NAME
+	String constraint_value; // value or column name
+};
+*/
