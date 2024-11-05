f96cb09a4ff33442cf323336daca655c4dbde880
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-05 22:02:24 +0600

    added new function

--- src/StringVectorConverter.cpp
+++ src/StringVectorConverter.cpp
@@ -56,3 +56,24 @@ public:
 		data += String("\n");
 		return data;
 	}
+	String vector2DToString(Vector<Vector<String>> vec_data, String delimiter) {
+		String data;
+
+		int num_rows = vec_data.get_size();
+		int num_columns = vec_data[0].get_size();
+
+		//extract a single line from a 1D Vector
+		for (int i=0; i<num_rows; i++) {
+            int j=0;
+            for(j; j<num_columns-1; j++) {
+                data += vec_data[i][j];
+                data += String(delimiter);
+            }
+			data += vec_data[i][j];
+            data += String("\n");
+		}
+		return data;
+	}
+};
+
+#endif
