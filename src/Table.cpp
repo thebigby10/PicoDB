a51c5592e37ff57ce64eb0e1b885333e8fe512ce
Muhtasim Zawad <muhtasimzawad.jfcl@gmail.com>
2024-11-05 22:33:45 +0600

    added private attributes in table class

--- src/Table.cpp
+++ src/Table.cpp
@@ -8,10 +8,28 @@
 using namespace std;
 
 /*
-class Constrains{
+class Constraints{
 public:
 	String constraint_key; // NOT_NULL, UNIQUE, PEIMARY_KEY, FOREIGN_KEY, DEFAULT, CHECK
 	String constraint_secondary_key; //LESS_THAN, GREATER_THAN, EQUAL_TO, NOT_EQUAL_TO, TABLE_NAME
 	String constraint_value; // value or column name
 };
 */
+
+class Table{
+private:
+	String table_name;
+	Vector<String> headers;
+	Vector<String> data_types;
+	Vector<String> constraints;
+	Vector<Vector<Cell>> table_data;
+
+	/*
+		String constrain_key;
+		// NOT_NULL, UNIQUE, PEIMARY_KEY, FOREIGN_KEY, DEFAULT, CHECK
+		String constrain_secondary_key;
+		//LESS_THAN, GREATER_THAN, EQUAL_TO, NOT_EQUAL_TO, TABLE_NAME
+		String constrain_value;
+		// value or column name
+	*/
+
