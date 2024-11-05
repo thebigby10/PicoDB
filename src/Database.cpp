18526b9c29108bb8b542afb8267ef9f43332bb78
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-05 22:09:44 +0600

    kore felsi

--- src/Database.cpp
+++ src/Database.cpp
@@ -20,3 +20,22 @@
 		//check if file exists
 		FileHandler conf_file = FileHandler(db_path+String("/")+db_name+String(".config")); //path to config file
 		if(conf_file.fileExists()){
+			// Conf manager fetches raw data ✅
+			ConfigManager conf_manager(db_path+String("/")+db_name+String(".config"));
+
+			// convert the config file into variables
+			this->delimiter = conf_manager.get_t_delimiter();
+			this->admin = conf_manager.get_admin();
+			//this->db_data = conf_manager.get_table_header();
+			// TODO : must check if the user exists
+
+			//convert the tables into vector ✅
+			loadCurrentTables(db_name, db_path);
+		}
+		else{
+			//create config file
+			ConfigManager conf_manager(db_path+String("/")+db_name+".config");
+			conf_manager.createConfig(db_name,table_delimiter,username );
+		}
+	};
+	// Database e(string db_name, bool encryption);
