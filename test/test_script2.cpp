3dbc19c094b27b47adde33b2632e99840f06094d
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-09 12:05:00 +0600

    testing

--- test/test_script2.cpp
+++ test/test_script2.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
     // Create or use existing database
-    PicoDB rapidb("studentdb", "/home/thebigby01/Codes/_github_repos/PicoDB/test/studentdb/", "thebigby01", "hello111", ",_,");
+    PicoDB rapidb("rapidb", "/Users/musaddiqrafi/Desktop/codes/3rdSem/SPL project/PicoDB/test/rapidb", "rapi", "hello111", ",_,");
     
     // Create a table named "person_info"
     rapidb.createTable(String("person_info"), {
@@ -21,11 +21,17 @@ int main() {
 
     // Insert data into the "person_info" table
     // Here we can assume you have a method to insert data, such as:
-    //rapidb.insert("person_info", {{"1", "Alice"}, {"2", "Bob"}, {"3", "Charlie"}});
+    // Insert data into the "person_info" table
+    rapidb.insertInto("person_info", {"id", "name"}, {"1", "Alice"});
+    rapidb.insertInto("person_info", {"id", "name"}, {"2", "Bob"});
+    rapidb.insertInto("person_info", {"id", "name"}, {"3", "Charlie"});
+    rapidb.insertInto("person_info", {"id", "name"}, {"4", "David"});
+    rapidb.insertInto("person_info", {"id", "name"}, {"5", "Eve"});
+
 
     // Select from the "person_info" table
     //Table selectedTable = 
-	rapidb.select("person_info", {});
+	//rapidb.select("person_info", {});
 
     // Print the selected table
     //
