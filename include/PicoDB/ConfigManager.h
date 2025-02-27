40c06f90fa287a774dfc5e9debf9aa77276c7d90
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2025-02-27 08:41:43 +0600

    updated ConfigManager and FileHandler for the new structure

---
+++ include/PicoDB/ConfigManager.h
@@ -0,0 +1,48 @@
+#ifndef MANAGECONFIG_H // Include guard start
+#define MANAGECONFIG_H
+
+#include "String.h"
+#include "Map.h"
+#include "Vector.h"
+#include "FileHandler.h"
+#include "Table.h"
+#include <fstream>
+
+class ConfigManager{
+private:
+	String conf_path;
+	String conf_data;
+public:
+	ConfigManager(String file_path){
+		this->conf_path = file_path;
+		FileHandler conf_file(conf_path);
+		if(conf_file.fileExists()){
+			this->conf_data = conf_file.readFromFile();
+		}
+
+	}
+	void createConfig(String database_name, String table_delimiter, String admin_username, bool encryption = true );
+
+	void updateConfig(String new_config_data);
+
+	void appendConfig(String new_config_data, bool append);
+
+	String get_t_delimiter() ;
+
+    String get_admin() ;
+
+    /*
+	Map<String, Vector<String>>  get_users()*/
+
+	bool is_encrypted();
+
+	Vector<Vector<String>> get_table_meta_data();
+
+
+	Vector<String> get_table_names() ;
+
+	void deleteConfig();
+
+};
+
+#endif
---
+++ include/PicoDB/FileHandler.h
@@ -0,0 +1,27 @@
+#ifndef FILEUTILS_H // Include guard start
+#define FILEUTILS_H
+#include "String.h"
+#include <fstream>
+
+class FileHandler{
+private:
+    String filepath;
+public:
+//checks if a file exists
+    FileHandler(String filepath){
+        this->filepath = filepath;
+    }
+
+    bool fileExists() ;
+// Function to read from a file and return a single String containing all its contents
+    String readFromFile() ;
+    // Function to write data to a file (same as before)
+    // The 'append' flag determines if the data should overwrite (false) or append (true) to the file
+    void writeToFile(const String& data, bool append = false) ;
+    // Function to create a file (it will overwrite if the file already exists)
+    void createFile() ;
+    // Function to delete a file
+    bool removeFile();
+};
+
+#endif
